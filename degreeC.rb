c9aa1a34953c95dfb217cc3d4083c2efef2c0b5b
Hugo <santiquiu@gmail.com>
2023-08-04 17:20:12 -0500

    add new classes: new AppLogical and Farenheit

--- degreeC.rb
+++ degreeC.rb
@@ -1,29 +1,47 @@
 name = gets.split(" ")
 
-if name.length() > 7
-  puts "Introduzca de nuevo el número de caracteres separados por dígitos"
-end
-
-my_hash = {
-  "Mon" => nil,
-  "Tue" => nil,
-  "Wed" => nil,
-  "Thu" => nil,
-  "Fri" => nil,
-  "Sat" => nil,
-  "Sun" => nil,
-}
-
-name.each_with_index do |value, index|
-  my_hash[my_hash.keys[index]] = value
-end
-
-=begin
-name.each do |num|
-  my_hash.each_key { |key| key => num }
+class AppLogical
+  attr :num, :my_hash      
+  def  initialize(num)
+    @num = num
   end
-=end
-#puts my_hash
+  
+  def filled
+      my_hash = {
+        "Mon" => nil,
+        "Tue" => nil,
+        "Wed" => nil,
+        "Thu" => nil,
+        "Fri" => nil,
+        "Sat" => nil,
+        "Sun" => nil,
+      }
+
+      num.each_with_index do |value, index|
+            my_hash[my_hash.keys[index]] = value
+      end
+      my_hash
+  end
+end
+
+class Farenheit
+  attr :temp       
+  def  initialize(temp)
+    @temp = temp.to_i
+  end
+
+  def degree_c 
+    temp*1.8 - 32
+  end
+  
+  def temp
+    @temp 
+  end
+                                
+  def prompt(week_day)
+    "#{week_day} | #{temp} degrees F | #{degree_c} desgrees C"
+  end
+end
 
 class Celcius
   def  initialize(temp)
@@ -39,8 +57,18 @@ class Celcius
   #  end
 end
 
-my_hash.each do |k,v| 
-  cel = Celcius.new(v)
-  f = cel.DegreeF 
-  puts "#{k} | #{v} degrees C | #{f.round()} desgrees F"
+app_log = AppLogical.new(name)
+
+#convert_to_degree F
+#app_log.filled.each do |k,v| 
+#  cel = Celcius.new(v)
+#  puts cel.prompt(k)
+#end
+
+puts"------------------------------------"  
+
+#convert_to_degree C
+app_log.filled.each do |k,v| 
+  cel = Farenheit.new(v)
+  puts cel.prompt(k)
 end
